b0da87e856238e3c747021441c0f897fa38e8e69
SW <szymonwojcik800@gmail.com>
2021-01-01 21:08:07 +0100

    Added mockup dashboard

--- react-app/src/components/Template/Footer.js
+++ react-app/src/components/Template/Footer.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      <p>Footer</p>
+      <footer class="mt-auto text-white-50 bg-dark text-center">
+          <br/>
+        
+        <p>Copyright &copy; 2021</p>
+          <br/>
+
+      </footer>
     )
   }
 }
