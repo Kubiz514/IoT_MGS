5e1300ee2a4a7ffaf5e31de5f224f39358014d59
SW <szymonwojcik800@gmail.com>
2021-01-02 21:12:09 +0100

    Added API routing

--- api/src/app.js
+++ api/src/app.js
@@ -7,20 +7,51 @@ const api_url = '/api'
 const port = 8080
 
 
-//Return all devices
+//Use Express JSON parser
+app.use(express.json())
+
+//GET all devices
 app.get(api_url+'/devices', (req, res) => {
   const devices = db_manager.get_devices()
+
   res.json(devices)
 })
 
+//GET device by id
+app.get(api_url+'/devices/:id', (req, res) => {
+    const id = req.params.id
+    const response = db_manager.get_device(id)
+
+    res.json(response)
+  }
+)
+
+//POST new device
+app.post(api_url+'/devices', (req, res) => {
+  //Express JSON parser
 
 
+  response = db_manager.add_device(req.body.id, req.body.name, req.body.on_off, req.body.device_status)
+  res.json(response)
+
+})
+
+//POST new device
+app.put(api_url+'/devices/:id', (req, res) => {
 
 
+    response = db_manager.update_device(req.body.id, req.body.name, req.body.on_off, req.body.device_status)
+    res.json(response)
 
+})
 
-
-
+//GET device by ID
+app.delete(api_url+'/devices/:id', (req, res) => {
+    const id = req.params.id
+    const response = db_manager.delete_device(id)
+    res.json(response)
+  }
+)
 
 
 app.listen(port, () => {
--- api/src/db_manager.js
+++ api/src/db_manager.js
@@ -1,3 +1,4 @@
+//Mockup devices list
 const mockup_devices_list = {
  devices: [
    { id: 1, name: "device 1",on_off: "on", state: "online" },
@@ -9,20 +10,30 @@ const mockup_devices_list = {
 
 function get_devices () {
     //DB logic
+
     const devices = mockup_devices_list
     return devices
   }
 
 function get_device (id) {
   //DB logic
-  const device = mockup_devices_list[0]
+  const device = id
   return device
 }
 
-function remove_device(id) {
+function add_device (id, name, on_off, device_status) {
+  //DB Logic
+  return 'Device added: '+'id: '+id+', '+'name: '+name+', '+'on_off: '+on_off+', '+'device_status: '+device_status
+}
+function update_device (id, name, on_off, device_status) {
+  //DB Logic
+  return 'Device updated: '+'id: '+id+', '+'name: '+name+', '+'on_off: '+on_off+', '+'device_status: '+device_status
+}
+
+function delete_device(id) {
   //DB logic
-  const device = mockup_devices_list[0]
-  return device
+
+  return 'Device removed'
 }
 
-module.exports =  { get_devices, get_device } ;
+module.exports =  { get_devices, get_device, add_device, update_device, delete_device } ;
