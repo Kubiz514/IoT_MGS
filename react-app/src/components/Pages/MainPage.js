dc0b0241651e7d73ec4906ee9f87c58ab50ad0fd
Kubiz514 <kubaploch34@gmail.com>
2021-01-07 23:32:00 +0100

    Added main page.

--- react-app/src/components/Pages/MainPage.js
+++ react-app/src/components/Pages/MainPage.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class MainPage extends Component {
   render() {
     return(
-      <h1>Main page</h1>
+      <div class="container">
+        <div class="row">
+            <div class="col vh-100 pt-5 text-white text-center align-items-center">
+              <div>
+                <h1>Manage your own IoT system.</h1>
+                <p>subheading: tutorial?</p>
+                <button type="submit" class="btn btn-primary mt-3">Log in</button>
+                <p></p>
+              </div>
+            </div>
+          </div>
+      </div>
     )
   }
 }
